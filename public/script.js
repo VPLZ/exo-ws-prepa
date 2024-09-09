8e68fd01623df3d8370a50f05d3e755d9e492d5e
Plazma <vdzlucas@gmail.com>
2024-09-09 15:04:48 +0200

    Radar chart

--- public/script.js
+++ public/script.js
@@ -1,27 +1,30 @@
-const chart = document.getElementById('chart').getContext('2d');
-const chart_data = {
-  labels: [
-    'Math',
-    'Sciences',
-    'Écologie'
-  ],
-  datasets: [{
-    label: 'Michel',
-    data: [10,15,3],
-    fill:true,
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgb(255, 99, 132)',
-    borderWidth: 3,
-    pointBackgroundColor: 'rgb(255, 99, 132)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(255, 99, 132)'
-  }]
-};
-
-const config = {
-  type: 'radar',
-  data: chart_data,
-  options: {}
-};
-new Chart(chart,config);
+const ctx = document.getElementById('chart');
+const myChart = new Chart(ctx, {
+  type: "radar",
+  data: {
+    labels: ["zeon","zeon","zeon","zeon"],
+    datasets:[{
+      label: "Forma 1",
+      data: ["40","65","35","96"],
+      fill: true,
+      backgroundColor: "blue"
+    }]
+  },
+  options:{
+    elements:{
+      line:{
+        borderWidth: 5
+      }
+    },
+    scales: {
+      r: {
+        min: 0,
+        max: 100,
+        ticks: {
+          stepSize: 20,
+          display : true
+        }
+      }
+    }
+  }
+});
